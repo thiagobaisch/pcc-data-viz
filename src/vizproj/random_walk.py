2a272f375c0df1ebf09a5ff86bc478c3fb7713be
TBaisch <thiagobaisch@gmail.com>
2025-08-17 09:33:11 -0300

    refactored random_walk

--- src/vizproj/random_walk.py
+++ src/vizproj/random_walk.py
@@ -11,18 +11,19 @@ class RandomWalk:
         self.x_values = [0]
         self.y_values = [0]
 
+    def get_step(self):
+        """return a random step"""
+        direction = choice([1,-1])
+        distance = choice([0, 1, 2, 3, 4])
+        return direction * distance
+
     def fill_walk(self):
         """Calculate all walk points"""
         
         while len(self.x_values) < self.num_points:
             #Decides direction
-            x_direction = choice([1,-1])
-            x_distance = choice([0, 1, 2, 3, 4])
-            x_step = x_direction * x_distance
-
-            y_direction = choice([1, -1])
-            y_distance = choice([0, 1, 2, 3, 4])
-            y_step = y_direction * y_distance
+            x_step = self.get_step()
+            y_step = self.get_step()
 
             #Reject stop movements
             if x_step == 0 and y_step == 0:
